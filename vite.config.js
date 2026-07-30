fe2de492a6660525c0259e49b98939f743a8d932
imel0n <hejieming09@gmail.com>
2026-07-30 14:38:25 +0800

    chore: remove vue dev tools

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
       // Lets the service worker be tested with `npm run dev`, not just after a build.
