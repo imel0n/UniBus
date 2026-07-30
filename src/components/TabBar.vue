922a0ae614607635ece639ab1ab11de244c41613
imel0n <hejieming09@gmail.com>
2026-07-30 12:10:57 +0800

    chore: set up blank pages and blank TabBar

---
+++ src/components/TabBar.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Nearby Stops</RouterLink>
+    <RouterLink to="/favourites">Favourite Stops</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+}
+
+nav a {
+  flex: 1;
+  text-align: center;
+}
+</style>
